ffa23ef3292f9f727276a0fbf9eafe4aff0c73eb
Emperor-One <101866976+Emperor-One@users.noreply.github.com>
2023-08-17 07:50:41 +0300

    Create hackerRank_swap_case.py

---
+++ hackerRank_swap_case.py
@@ -0,0 +1,14 @@
+def swap_case(s):
+    swapped_string = ''
+    for char in s:
+        if char.islower():
+            swapped_string += char.upper()
+        
+        else:
+            swapped_string += char.lower()
+    return swapped_string
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
